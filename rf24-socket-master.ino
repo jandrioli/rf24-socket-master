34ac0160c682ec219d872da3fa4f30e0cf89a8c6
Carlinhos <carlos@localhost>
2018-01-21 16:16:46 -0500

    2018Jan21-1616 by script

--- rf24-socket-master.ino
+++ rf24-socket-master.ino
@@ -23,47 +23,46 @@
 // Set up nRF24L01 radio on SPI bus plus pins 8 & 9
 RF24 radio(9,10);
 
-// Radio pipe addresses for the 2 nodes to communicate.
-// WARNING!! 3Node and 4Node are used by my testing sketches ping/pong
-const uint8_t addresses[][5] = {
-  "0Node", // master writes broadcasts here
-  "1Node", // unor3 writes here
-  "2Node", // unor3 reads here
-  "3Node", // arrosoir reads here
-  "4Node", // arrosoir writes here
-  "5Node"};// not yet used by anybody
+// Use multicast?
+// sets the multicast behavior this unit in hardware.  Connect to GND to use unicast
+// Leave open (default) to use multicast.
+const int multicast_pin = 6 ;
 
-  
-/**
- * exchange data via radio more efficiently with data structures.
- * we can exchange max 32 bytes of data per msg. 
- * schedules are reset every 24h (last for a day) so an INTEGER is
- * large enough to store the maximal value of a 24h-schedule.
- * temperature threshold is rarely used
- */
-struct relayctl {
-  unsigned long uptime = 0;                      // current running time of the machine (millis())  4 bytes  
-  unsigned long sched1 = DEFAULT_ACTIVATION;     // schedule in minutes for the relay output nbr1   4 bytes
-  unsigned long sched2 = DEFAULT_ACTIVATION;     // schedule in minutes for the relay output nbr2   4 bytes
-  unsigned int  maxdur1 = DEFAULT_DURATION;      // max duration nbr1 is ON                         2 bytes
-  unsigned int  maxdur2 = DEFAULT_DURATION;      // max duration nbr2 is ON                         2 bytes
-  unsigned int  temp_thres = 999;                // temperature at which the syatem is operational  4 bytes
-  float         temp_now   = 20;                 // current temperature read on the sensor          4 bytes
-  short         battery    =  0;                 // current temperature read on the sensor          2 bytes
-  bool          state1 = false;                  // state of relay output 1                         1 byte
-  bool          state2 = false;                  // "" 2                                            1 byte
-  bool          waterlow = false;                // indicates whether water is low                  1 byte
-  byte          nodeid = THIS_NODE_ID;           // nodeid is the identifier of the slave           1 byte
-} myData;
+// sets the role of this unit in hardware.  Connect to GND to be the 'pong' receiver
+// Leave open to be the 'ping' transmitter
+const int role_pin = 7;
+bool multicast = true ;
+
+//
+// Topology
+//
+
+// Radio pipe addresses for the 2 nodes to communicate.
+//const uint64_t pipes[2] = { 0xEEFAFDFDEELL, 0xEEFDFAF50DFLL };
+const uint8_t pipes[][6] = {"1Node","2Node"};
 
 void setup(void)
 {
+  //
+  // Multicast
+  //
+  pinMode(multicast_pin, INPUT);
+  digitalWrite(multicast_pin,HIGH);
+  delay( 20 ) ;
+
+  // read multicast role, LOW for unicast
+  if( digitalRead( multicast_pin ) )
+    multicast = true ;
+  else
+    multicast = false ;
 
   //
   // Print preamble
   //
   Serial.begin(115200);
   Serial.println(F("RF24 Master - power socket controller"));  
+  Serial.print(F("MULTICAST: "));
+  Serial.println(multicast ? F("true (unreliable)") : F("false (reliable)"));
   
   //
   // Setup and configure rf radio
@@ -73,10 +72,8 @@ void setup(void)
   // optionally, increase the delay between retries & # of retries
   radio.setRetries( 15, 5 ) ;
   radio.setAutoAck( true ) ;
-  radio.setPALevel( RF24_PA_MAX ) ;
-  radio.setDataRate( RF24_250KBPS ) ;
-  radio.setChannel( 108 ) ;
-  radio.enableDynamicPayloads(); //dont work with RPi :-/
+  //radio.setPALevel( RF24_PA_LOW ) ;
+  //radio.enableDynamicPayloads(); //dont work with RPi :-/
 
   //
   // Open pipes to other nodes for communication
@@ -85,8 +82,8 @@ void setup(void)
   // back and forth.
   // Open 'our' pipe for writing
   // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-  radio.openWritingPipe(pipes[1]);
-  radio.openReadingPipe(1,pipes[0]);
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
 
 
   //
@@ -170,7 +167,7 @@ bool queryString(char theQuery[], uint8_t queryLength, String& oValue)
 {
 
   // send query
-  radio.write( theQuery, queryLength, false );
+  radio.write( theQuery, queryLength, multicast );
 
   // Now, continue listening
   radio.startListening();
